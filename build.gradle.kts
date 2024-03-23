e1344d69355201ad65769f27a36ac869fb55a356
Stefan Schaar <stefan.schaar@tuta.io>
2024-03-23 22:32:27 +0100

    apiKotlinExample

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+val ktor_version: String by project
 
 repositories {
     mavenCentral()
@@ -11,6 +12,9 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,41 @@
-package org.example
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonObject
+import java.io.File
 
-fun main() {
-    println("Hello World!")
+// Function to load the token from the text file
+fun loadTokenFromFile(filePath: String): String? {
+    return try {
+        File(filePath).readText().trim()
+    } catch (e: Exception) {
+        println("Error loading the token: ${e.message}")
+        null
+    }
+}
+
+suspend fun main() {
+
+    val tokenFilePath = "rsc/token.txt"
+    val token = loadTokenFromFile(tokenFilePath)
+
+    val city = "Vienna"
+
+    val client = HttpClient(CIO)
+
+    val response: HttpResponse = client.get("http://api.weatherapi.com/v1/current.json?key=$token&q=$city")
+    val body: String = response.body()
+    val json = Json.decodeFromString<JsonObject>(body)
+
+    // To print the whole JSON-Object
+    println(json)
+
+    // To print a specific part of the JSON-Object
+    println(json["location"]?.jsonObject!!["name"])
+
+    client.close()
 }
